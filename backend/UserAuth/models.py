3a186a76e850ce9d640483be8ba1f0669726deec
Rijan Gautam <rijangautam07@gmail.com>
2025-10-28 20:47:47 +0545

    AuthUser app created, CustomUserManager and CustomUser created

---
+++ backend/UserAuth/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin, Group, Permission
+
+class CustomUserManager(BaseUserManager): 
+    
+    def create_user(self, username: str, email: str, password: str, **extra_fields):
+        if not email: 
+            raise ValueError("Email required to login")
+        
+        email = self.normalize_email()
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, username: str, email:str, password: str, **extra_fields): 
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        self.create_user(username=username, email=email, password=password, **extra_fields)
+        
+class CustomeUser(AbstractBaseUser, PermissionsMixin): 
+    
+    email = models.CharField(max_length=225, unique=True)
+    username = models.CharField(max_length=225)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    
+    groups = models.ManyToManyField(
+        Group, 
+        related_name="customuser_set", 
+        blank=True
+    )
+    
+    user_permissions = models.ManyToManyField(
+        Permission, 
+        related_name="customuser_permissions_set", 
+        blank=True, 
+    )
+    
+    objects = CustomUserManager()
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    
+    def __str__(self): 
+        return self.username
