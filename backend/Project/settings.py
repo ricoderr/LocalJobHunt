c2211d36f50af675a0299836aefbf6dcdc626492
Rijan Gautam <rijangautam07@gmail.com>
2025-10-31 15:08:51 +0545

    Static files collected for deployment

--- backend/Project/settings.py
+++ backend/Project/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
 ]
 
 ROOT_URLCONF = 'Project.urls'
@@ -73,6 +74,10 @@ TEMPLATES = [
         },
     },
 ]
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 WSGI_APPLICATION = 'Project.wsgi.application'
 
